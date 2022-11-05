7d03413b26e6620abd0057b8aa48d80f5b406935
Aldry Albuquerque <albuquerquealdry@gmail.com>
2022-11-05 16:26:22 -0300

    fix. add port env

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,15 @@
 package routes
 
 import (
+	"os"
+
 	"github.com/albuquerquealdry/chatops-babidi/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func Request() {
 	r := gin.Default()
-	r.POST("/commitAction", controllers.CommitAction)
-	r.Run(":5000")
 
+	r.POST("/commitAction", controllers.CommitAction)
+	r.Run(os.Getenv("PORT"))
 }
