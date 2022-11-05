fb7977023c69c8abd481d1ccef8bae99e4807f68
Aldry Albuquerque <albuquerquealdry@gmail.com>
2022-11-05 14:37:41 -0300

    add. resolver lint

--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,13 @@
 package routes
 
-import "github.com/gin-gonic/gin"
-import "github.com/albuquerquealdry/chatops-babidi/controllers"
+import (
+	"github.com/albuquerquealdry/chatops-babidi/controllers"
+	"github.com/gin-gonic/gin"
+)
 
 func Request() {
 	r := gin.Default()
-	r.GET("/commitAction", controllers.CommitAction)
+	r.POST("/commitAction", controllers.CommitAction)
 	r.Run(":3001")
+
 }
