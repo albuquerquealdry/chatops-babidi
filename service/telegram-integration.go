da13652b2afd7f4bca7a689553f54c10aa9180d9
Aldry Albuquerque <85401149+albuquerquealdry@users.noreply.github.com>
2022-11-05 18:32:11 -0300

    fix. update code develop

--- service/telegram-integration.go
+++ service/telegram-integration.go
@@ -13,7 +13,7 @@ import (
 func TelegramRander(repository string, commitHash string, branch string, user string, timestamp string) {
 	godotenv.Load(".env")
 	//token_telegram := os.Getenv("API_TOKEN")
-	text := fmt.Sprintf("	🔮Babidi Chatops🔮\n \n🏟️ Repository : %s \n🏟️ Commit : %s\n📓Branch : %s\n👤User : %s\n🕒Timestamp : %s ", repository, commitHash, branch, user, timestamp)
+	text := fmt.Sprintf("	🔮Babidi Chatops🔮\n \n 📦 Repository : %s \n🏟️ Commit : %s\n📓Branch : %s\n👤User : %s\n🕒Timestamp : %s ", repository, commitHash, branch, user, timestamp)
 	called := fmt.Sprintf("https://api.telegram.org/bot5757162669:AAEXlI_N-mNAitaKzBO5w7TTo7N3Cb73z0E/sendMessage?chat_id=-866409625&text=%s", url.QueryEscape(text))
 	resp, err := http.Get(called)
 
