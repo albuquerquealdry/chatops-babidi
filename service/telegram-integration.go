6d34f8a3b77e61e49721ae64fe3cfe1b2fd7a87b
Aldry Albuquerque <albuquerquealdry@gmail.com>
2022-11-06 12:20:53 -0300

    add. Pull method

--- service/telegram-integration.go
+++ service/telegram-integration.go
@@ -9,9 +9,26 @@ import (
 	"os"
 )
 
-func TelegramRander(repository string, commitHash string, branch string, user string, timestamp string) {
+func TelegramCommit(repository string, commitHash string, branch string, user string, timestamp string) {
 	var token = os.Getenv("TOKEN_TELEGRAM")
-	text := fmt.Sprintf("	🔮Babidi Chatops🔮\n \n 📦 Repository : %s \n🏟️ Commit : %s\n📓Branch : %s\n👤User : %s\n🕒Timestamp : %s ", repository, commitHash, branch, user, timestamp)
+	text := fmt.Sprintf("	🔮Babidi Chatops🔮\n \n Action:Commit🎈 \n-------------------------------------\n \n📦 Repository : %s \n🏟️ Commit : %s\n📓Branch : %s\n👤User : %s\n🕒Timestamp : %s ", repository, commitHash, branch, user, timestamp)
+	called := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=-866409625&text=%s", token, url.QueryEscape(text))
+	resp, err := http.Get(called)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
+}
+
+func TelegramPull(repository string, commitHash string, branch string, user string, timestamp string) {
+	var token = os.Getenv("TOKEN_TELEGRAM")
+	text := fmt.Sprintf("	🔮Babidi Chatops🔮\n \n Action:Pull Request🔄 \n-------------------------------------\n📦 Repository : %s \n⭐ OriginBranch : %s\n🌌 FutureBranch : %s\n👤User : %s\n🕒Timestamp : %s ", repository, commitHash, branch, user, timestamp)
 	called := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=-866409625&text=%s", token, url.QueryEscape(text))
 	resp, err := http.Get(called)
 
